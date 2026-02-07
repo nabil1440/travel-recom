a89cebbfadb20341e8e6c08d748fa9e448413663
Nabil <nabilaltamash@gmail.com>
2026-02-07 17:13:13 +0600

    chore(migrations): add aggregation date and leaderboard snapshots

---
+++ src/Infrastructure/Migrations/20260207111228_ClarifyAggregationDateAndLeaderboardSnapshots.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ClarifyAggregationDateAndLeaderboardSnapshots : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "district_weather_snapshots",
+                newName: "date");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_district_weather_snapshots_DistrictId_Date",
+                table: "district_weather_snapshots",
+                newName: "IX_district_weather_snapshots_DistrictId_date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "date",
+                table: "district_weather_snapshots",
+                newName: "Date");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_district_weather_snapshots_DistrictId_date",
+                table: "district_weather_snapshots",
+                newName: "IX_district_weather_snapshots_DistrictId_Date");
+        }
+    }
+}
